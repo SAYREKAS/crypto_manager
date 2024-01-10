85d905a5fa9b4719e8c27e86b16dc3dd988d36a6
SAYREKAS <chaotichoice@gmail.com>
2024-01-10 15:29:05 +0200

    парсер із сесією для прискорення роботи

--- parser.py
+++ parser.py
@@ -2,7 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_coin_info(user_coin_name):
+def get_coin_info(coin_name_list):
+    list1 = []
+    try:
+        for f in coin_name_list:
+            url = f'https://coinmarketcap.com/currencies/{f}/'
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
+
+            with requests.Session() as session:
+                session.headers.update(headers)
+                req = session.get(url).text
+                bs = BeautifulSoup(req, 'lxml')
+
+                coin_info = bs.find('h1', class_="sc-f70bb44c-0 gYiXVQ").text.split()
+                coin_price = float(
+                    bs.find('span', class_="sc-f70bb44c-0 jxpCgO base-text").text.replace('$', '').replace(',', ''))
+                coin_name = coin_info[0]
+                coin_symbol = coin_info[2]
+                list1.append([coin_name, coin_symbol, coin_price])
+        return list1
+    except AttributeError:
+        return False
+
+
+def check_for_exis_coin(user_coin_name):
     try:
         url = f'https://coinmarketcap.com/currencies/{user_coin_name}/'
         headers = {
@@ -18,4 +42,3 @@ def get_coin_info(user_coin_name):
         return coin_name, coin_symbol, coin_price
     except AttributeError:
         return False
-
