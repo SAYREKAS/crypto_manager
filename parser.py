0d26f20b09ffb8fefd12e553ab55cb5910183733
SAYREKAS <chaotichoice@gmail.com>
2024-02-03 13:04:25 +0200

    1. функцію get_percent_change() видалено а її функціонал перенесено в get_coin_info() 2. функція get_coin_info() тепер додатково повертає 'tags' і 'percent_change' для монети 3. збільшено ліміт монет по яким парситься інформація з 200 до 400 4. оптимізація коду для покращення читабельності

--- parser.py
+++ parser.py
@@ -3,7 +3,7 @@ import time
 import requests
 from db import add_coin_to_db
 
-coin_limit: int = 200
+coin_limit: int = 400
 update_period: int = 60
 
 url = (f'https://api.coinmarketcap.com/data-api/v3/cryptocurrency/listing?start=1&limit={coin_limit}&sortBy=market_cap'
@@ -55,37 +55,28 @@ def check_for_exist_coin(coin_name_or_symbol):
 
 
 def get_coin_info(coin_name_list):
+    percent_change = ()
     data = response()
     coin_data = []
-    for coin_info in data.get('data', {}).get('cryptoCurrencyList', []):
-        name = coin_info['name']
-        symbol = coin_info['symbol']
-        price = round(float(coin_info['quotes'][2]["price"]), 2)
 
-        if name.lower() in coin_name_list:
-            coin_data.append({'name': name, 'symbol': symbol, 'price': price})
+    for coin_info in data.get('data').get('cryptoCurrencyList'):
+
+        for quota in coin_info["quotes"]:
+            if quota['name'] != 'USD':
+                continue
+            percent_change = (
+                round(float(quota['percentChange1h']), 2),
+                round(float(quota['percentChange24h']), 2),
+                round(float(quota['percentChange7d']), 2),
+                round(float(quota['percentChange30d']), 2),
+                round(float(quota['percentChange60d']), 2),
+                round(float(quota['percentChange90d']), 2),
+            )
+
+        if coin_info['name'].lower() in coin_name_list:
+            coin_data.append(
+                {'name': coin_info['name'], 'symbol': coin_info['symbol'], 'tags': coin_info['tags'],
+                 'price': round(float(coin_info['quotes'][2]["price"]), 2), 'percent_change': percent_change})
 
     coin_data = sorted(coin_data, key=lambda x: x['name'])
-    return coin_data
-
-
-def get_percent_change(coin_name):
-    coin_data = ()
-    with open('coin info.json', 'r', encoding='utf8') as file:
-        data = json.load(file)
-        for f in data.get("data").get("cryptoCurrencyList"):
-
-            if str(f['name']).lower() != str(coin_name).replace('_', ' ').lower():
-                continue
-            for n in f["quotes"]:
-                if n['name'] != 'USD':
-                    continue
-                coin_data = (
-                    round(float(n['percentChange1h']), 2),
-                    round(float(n['percentChange24h']), 2),
-                    round(float(n['percentChange7d']), 2),
-                    round(float(n['percentChange30d']), 2),
-                    round(float(n['percentChange60d']), 2),
-                    round(float(n['percentChange90d']), 2),
-                )
     return coin_data
